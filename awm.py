db1a5bcefee3ce6fe5e799c56364d05002f7946f
Andrei Amatuni <andrei.amatuni@gmail.com>
2016-01-19 11:58:51 -0500

    check timestamps

--- awm.py
+++ awm.py
@@ -34,10 +34,11 @@ def parse_new_file(type):
             for row in reader:
                 new_audiofile_data.append(row)
 
-# def merge_audio():
-#
-# def merge_video():
-#
+def merge_audio():
+    print "hello"
+
+def merge_video():
+    print "hello"
 
 def figure_out_filetype(file):
     with open(file, "rU") as file:
@@ -57,6 +58,16 @@ def figure_out_filetype(file):
     if audio_csv:
         return "audio"
 
+def check_audio_timestamps():
+    old_file_timestamps = [entry[5] for entry in old_audiofile_data]
+    new_file_timestamps = [entry[5] for entry in new_audiofile_data]
+    for index, timestamp in enumerate(old_file_timestamps):
+        if timestamp not in new_file_timestamps:
+            print "Timestamp at line: " + index + "  is only in the old file. Not found in new file\n"
+    for index, timestamp in enumerate(new_file_timestamps):
+        if timestamp not in old_file_timestamps:
+            print "Timestamp at line: " + index + "  is only in the new file. Not found in old file\n"
+            
 if __name__ == "__main__":
 
     old_file = sys.argv[1]
@@ -67,10 +78,17 @@ if __name__ == "__main__":
 
     if old_file_type != new_file_type:
         print "old file and new file are of different types"
+        print "old file = " + old_file_type
+        print "new_file = " + new_file_type
         sys.exit(0)
 
     parse_old_file(old_file_type)
     parse_new_file(new_file_type)
 
-    print old_audiofile_data
-    print new_audiofile_data
+    print "\nold_file_type = " + old_file_type
+    print "new_file_type = " + new_file_type
+
+    if old_file_type == "video":
+        merge_video()
+    else:
+        merge_audio()
